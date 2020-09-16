ef5cfd6dee63e96dd9121a4205a2ec6f2325e0a2
Jaqueline <jakiduque@gmail.com>
2020-09-16 19:54:06 -0300

    Including profile data

--- main.go
+++ main.go
@@ -27,8 +27,9 @@ func init() {
 		RedirectURL:  "http://localhost:8080/callback",
 		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
 		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
-		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
-		Endpoint:     google.Endpoint,
+		Scopes: []string{"https://www.googleapis.com/auth/userinfo.email",
+			"https://www.googleapis.com/auth/userinfo.profile"},
+		Endpoint: google.Endpoint,
 	}
 }
 
